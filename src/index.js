32500ecdbc5211239dd2947d763b3dc4147eb05f
mina <takhminam@gmail.com>
2021-09-23 20:28:52 +0300

    Add external file status.js that contains method for changing the input checkbox

--- src/index.js
+++ src/index.js
@@ -1,43 +1,59 @@
 import _ from 'lodash'; // eslint-disable-line no-unused-vars
 import './style.css';
-import { changeStatus } from './status.js';
+import status from './status.js';
 
-const CHECKED = '<i class="bi bi-check-square"></i>';
 const INDEX = '<i class="bi bi-three-dots-vertical"></i>';
-const arrs = [
-  {
-    completed: true,
-    description: 'Buy milk from the market',
-    index: 0,
-  },
-  {
-    completed: false,
-    description: 'Prepare meal',
-    index: 1,
-  },
-];
+
+// status.arrs = [
+//   {
+//     completed: true,
+//     description: 'Buy milk from the market',
+//     index: 0,
+//   },
+//   {
+//     completed: false,
+//     description: 'Prepare meal',
+//     index: 1,
+//   },
+// ];
+// arrs = JSON.parse(localStorage.getItem('arr')) || [];
 
 const todoList = document.getElementById('todo-list');
-arrs.forEach((todoEl) => {
+
+status.arrs.forEach((todoEl) => {
   const div = document.createElement('div');
   div.setAttribute('class', 'elem');
 
   const divBtn = document.createElement('div');
   divBtn.setAttribute('id', 'btnCheck');
 
-  divBtn.innerHTML = '<input type="checkbox" id="myCheck" value = todoEl.completed>';
-  divBtn.addEventListener('change', changeStatus);
+  const input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('id', 'myCheck');
+  input.className = 'checkItem';
+  input.addEventListener('change', status.changeStatus);
+
+  // div.addEventListener('click', (e) => {
+  //   const id = e.target.className;
+  //   const getclasses = id.split(' ');
+  //   const item2BeRemoved = e.target.parentElement.parentElement;
+  //   const nodes = Array.from(todoList.children);
+  //   const index = nodes.indexOf(item2BeRemoved);
+  //   toggleBullet(arrs, index);
+  // });
 
   const newTask = document.createElement('p');
   newTask.innerText = todoEl.description;
 
+  const indx = document.createElement('div');
+  indx.setAttribute('class', 'verticalDots');
+  indx.innerHTML = INDEX;
+
+  divBtn.append(input);
   div.append(divBtn);
   todoList.append(div);
   div.append(newTask);
   todoList.append(div);
-  const indx = document.createElement('div');
-  indx.setAttribute('class', 'verticalDots');
-  indx.innerHTML = INDEX;
   div.append(indx);
   todoList.append(div);
 });
--- src/status.js
+++ src/status.js
@@ -1,7 +1,35 @@
-import { bindAll } from 'lodash';
+class Status {
+  constructor() {
+    this.arrs = [
+      {
+        completed: true,
+        description: 'Buy milk from the market',
+        index: 0,
+      },
+      {
+        completed: false,
+        description: 'Prepare meal',
+        index: 1,
+      },
+    ];
 
-function changeStatus() {
-  document.getElementById('btnCheck').firstElementChild.value = true;
+    this.arrs = JSON.parse(localStorage.getItem('arrs')) || [];
+  }
+
+  static changeStatus() {
+    const x = document.getElementById('myCheck').checked;
+    if (x) {
+      this.arrs.completed = !this.arrs.completed;
+    }
+    localStorage.setItem('arrs', JSON.stringify(this.arrs));
+    console.log(this.arrs);
+  }
+
+  // const toggleBullet = (arrs, id) => {
+  //   const bullet = arrs.find((task) => task.id === id);
+  //   if (bullet) {
+  //   bullet.completed = !bullet.completed;
+  //   }
+  // };
 }
-
-export { changeStatus };
+export default Status;
